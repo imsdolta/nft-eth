6b0088cf7f36c35596e29070d143c7871230c8c6
sachin dolta <imsdolta@gmail.com>
2021-10-10 18:25:21 +0530

    Add script to run and compile contract

---
+++ scripts/run.js
@@ -0,0 +1,16 @@
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('NFT');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log('Contract deployed to address', nftContract.address);
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
